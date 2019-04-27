a8ac5aff3204f5ac1e92f40c226a114a0b650e1d
xuyaoxiang <1124557724@qq.com>
2019-04-27 17:17:57 +0800

    add easysms

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Providers\EasySmsServiceProvider;
+
 return [
 
     /*
@@ -176,6 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\EasySmsServiceProvider::class,
 
     ],
 
